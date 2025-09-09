012535233951b230d988602807bb316140c50fc5
Mukul Kumar <mukulkumar7707@gmail.com>
2025-09-10 03:16:14 +0530

    env files given and dashboard updated

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const path = require('path');
 
 exports.home = (req,res,next)=>{
@@ -8,4 +9,17 @@ exports.addsoon = (req,res,next) => {
     res.sendFile(path.join(__dirname,'../','views','add-soon.html'));
 }
 
+exports.getuserdashboard = (req, res, next) => {
+    if (!req.session.isLoggedIn) {
+        const errors = ["Please log in to access the dashboard"];
+        return res.render('login', {
+            errors,
+            oldInput : {email : "" , password : ""}
+        });
+    }
+
+    res.render('userDash', {
+        user: req.session.user
+    });
+};
 
